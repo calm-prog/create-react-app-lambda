8206a1a24bea21cbe7052d1869e50ceda23f4022
calm-prog <61384685+calm-prog@users.noreply.github.com>
2021-04-18 20:13:37 +0200

    Completed main styling of Checkout and Confirmation components

--- src/presentation/middle/confirmation/Confirmation.js
+++ src/presentation/middle/confirmation/Confirmation.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Confirmation.css'
+import { useLocation } from 'react-router-dom'
+import firebase from '../../../firebase'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { AutorenewTwoTone } from '@material-ui/icons';
 
-const Confirmation = (props) => {
-    const {name, store} = props;
+const useStyles = makeStyles({
+    content: {
+      width: "60%",
+      margin: "auto"
+    },
+  });
+
+const Confirmation = () => {
+    const [confirmation, setConfirmation] = useState({name: "", email: "", store: ""});
+    const location = useLocation();
+    const ref = firebase.firestore().collection("orders");
+    const classes = useStyles();
+
+    useEffect(() => {
+        ref.doc(location.state).get().then((doc) => {
+            if (doc.exists) {
+                const {name, email, store} = doc.data();
+                setConfirmation({name: name.split(' ')[0], email: email, store: store})
+            } else {
+                // doc.data() will be undefined in this case
+            }        
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+    }, [location.state])
 
     return(
-        <React.Fragment >
-            <div className="confirmation-container">
-                <div>Thank you {name} for your order!</div>
-                <div>You can pick up your order at {store}.</div>
-            </div>
-        </React.Fragment>
+        <Card className={classes.content}>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    Thank you {confirmation.name} for your order!
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    You can pick up your order at {confirmation.store} with the following order ID: {location.state}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    We will send you notification to your email address{"(" + confirmation.email + ")"} once your order is ready for pickup.
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
--- src/shared/UI/Form.js
+++ src/shared/UI/Form.js
@@ -1,24 +1,40 @@
 import React, {useState, useEffect, useRef} from 'react'
 import './Form.css'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
+const useStyles = makeStyles({
+    input: {
+      margin: "1rem"
+    }
+  });
+  
 
 const Form = (props) => {
 
     const { formFields, formState, updateFormState } = props;
+
+    const classes = useStyles();
  
     return(
         <div className="form-container">
                 {formFields.map((field,index0) => {
                     switch(field.tag) {
                         case 'input':
-                            return <input key={index0} name={field.name} value={formState[field.name]} onChange={updateFormState} type={field.type} placeholder={field.name}/>
+                            return <TextField className={classes.input} key={index0} name={field.name} value={formState[field.name]} onChange={updateFormState} type={field.type} placeholder={field.name} label={field.name} variant="outlined"/>
                         case 'select':
-                            return (<select key={index0} name={field.name} onChange={updateFormState}>
-                                    <option name="default" value={null}>{"Select a " + field.name}</option> 
-                                    {field.options.map((option, index1) => (
-                                        <option key={index1} name={option} value={option}>{option}</option>
-                                    ))}
-                                    </select>)
+                            return (<FormControl className={classes.input} variant="outlined">
+                                        <InputLabel name="default" value={null}>{"Select a store"}</InputLabel> 
+                                        <Select key={index0} onChange={updateFormState} label="Select a store" name="store" value={formState[field.name]}>
+                                        {field.options.map((option, index1) => (
+                                            <MenuItem key={index1} value={option}>{option}</MenuItem>
+                                        ))}
+                                        </Select>
+                                    </FormControl>)
                         case 'custom':
                             return React.createElement(field.tagType, {key: index0.toString()}, field.content)
                         default:
