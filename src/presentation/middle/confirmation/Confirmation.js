f98b2ff43e484f47f9189700d92b18b80e6f5fa8
calm-prog <61384685+calm-prog@users.noreply.github.com>
2021-04-26 12:13:17 +0200

    Container width adjusted in Checkout and Confirmation

--- src/presentation/middle/confirmation/Confirmation.js
+++ src/presentation/middle/confirmation/Confirmation.js
@@ -8,18 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { AutorenewTwoTone } from '@material-ui/icons';
 
-const useStyles = makeStyles({
-    content: {
-      width: "60%",
-      margin: "auto"
-    },
-  });
-
 const Confirmation = () => {
     const [confirmation, setConfirmation] = useState({name: "", email: "", store: ""});
     const location = useLocation();
     const ref = firebase.firestore().collection("orders");
-    const classes = useStyles();
 
     useEffect(() => {
         ref.doc(location.state).get().then((doc) => {
@@ -35,19 +27,22 @@ const Confirmation = () => {
     }, [location.state])
 
     return(
-        <Card className={classes.content}>
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Thank you {confirmation.name} for your order!
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    You can pick up your order at {confirmation.store} with the following order ID: {location.state}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    We will send you a notification to your email address{"(" + confirmation.email + ")"} once your order is ready for pickup.
-                </Typography>
-            </CardContent>
-        </Card>
+        <div className="confirmation-container">
+            <Card>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Thank you {confirmation.name} for your order!
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        You can pick up your order at {confirmation.store} with the following order ID: {location.state}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        We will send you a notification to your email address{"(" + confirmation.email + ")"} once your order is ready for pickup.
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+
     )
 }
 
